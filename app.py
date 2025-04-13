d238dd210e7092a782ee10be57053d0afb72b7b0
صادق حمزه محمد <e092333054@s.uokerbala.edu.iq>
2025-04-14 02:04:42 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# المفتاح الخاص بالتشفير الأحادي (تقدر تغيّره حسب رغبتك)
+original_alphabet = 'abcdefghijklmnopqrstuvwxyz'
+substitution_alphabet = 'QWERTYUIOPASDFGHJKLZXCVBNM'.lower()
+
+def monoalphabetic_encrypt(plaintext):
+    encrypted = ''
+    for char in plaintext.lower():
+        if char in original_alphabet:
+            index = original_alphabet.index(char)
+            encrypted += substitution_alphabet[index]
+        else:
+            encrypted += char  # غير الحروف تُترك كما هي
+    return encrypted
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    encrypted_text = ''
+    if request.method == 'POST':
+        plaintext = request.form['plaintext']
+        encrypted_text = monoalphabetic_encrypt(plaintext)
+    return render_template('index.html', encrypted=encrypted_text)
+
+if __name__ == '__main__':
+    app.run(debug=True)
