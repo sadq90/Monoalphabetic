1b6a2be8f74e169dc1990b9389b70a891de48861
صادق حمزه محمد <e092333054@s.uokerbala.edu.iq>
2025-04-14 02:41:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -25,4 +25,5 @@ def index():
     return render_template('index.html', encrypted=encrypted_text)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
